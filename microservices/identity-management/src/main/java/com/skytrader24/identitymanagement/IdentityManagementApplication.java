f6672a8104cf149395e65aca2182652627420076
mcblankenburg <maciej.blankenburg@gmail.com>
2023-06-05 00:28:13 +0200

    skeleton for IdentityManagement

---
+++ microservices/identity-management/src/main/java/com/skytrader24/identitymanagement/IdentityManagementApplication.java
@@ -0,0 +1,12 @@
+package com.skytrader24.identitymanagement;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityManagementApplication.class, args);
+    }
+}
