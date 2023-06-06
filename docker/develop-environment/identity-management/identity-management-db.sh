514c913aa782084a25caa76edf6aa49fb3c7a899
mcblankenburg <maciej.blankenburg@gmail.com>
2023-06-06 23:53:42 +0200

    improved script to delete old container

--- docker/develop-environment/identity-management/identity-management-db.sh
+++ docker/develop-environment/identity-management/identity-management-db.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
+
+# Nazwa kontenera
+CONTAINER_NAME="identity-management-dev-db"
+
+# Sprawdzenie, czy kontener już istnieje
+if docker ps -a --format "{{.Names}}" | grep -q "^$CONTAINER_NAME$"; then
+    echo "Deleting old container: $CONTAINER_NAME"
+    docker stop $CONTAINER_NAME > /dev/null 2>&1
+    docker rm $CONTAINER_NAME > /dev/null 2>&1
+fi
+
+# Uruchomienie nowego kontenera
 docker run -d \
---name identity-management-dev-db \
+--name $CONTAINER_NAME \
 -e POSTGRES_USER=$SKYTRADER24_DEV_USERNAME \
 -e POSTGRES_PASSWORD=$SKYTRADER24_DEV_PASSWORD \
 -e POSTGRES_DB=identity-management-dev-db \
 -v $PWD/src/main/resources/db/:/docker-entrypoint-initdb.d \
 -p 5432:5432 \
 postgres
+echo "Created new container: $CONTAINER_NAME"
