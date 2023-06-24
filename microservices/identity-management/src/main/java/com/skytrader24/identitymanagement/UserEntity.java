20f8b0cd696184139b18f33c117f091af1628e77
mcblankenburg <maciej.blankenburg@gmail.com>
2023-06-24 16:18:11 +0200

    add userROle, isBlocked, creationDate and lastLoggedDate

--- microservices/identity-management/src/main/java/com/skytrader24/identitymanagement/UserEntity.java
+++ microservices/identity-management/src/main/java/com/skytrader24/identitymanagement/UserEntity.java
@@ -1,8 +1,10 @@
-package com.skytrader24.identitymanagement.entity;
+package com.skytrader24.identitymanagement;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
@@ -15,5 +17,9 @@ public class UserEntity {
     private String username;
     private String password;
     private String email;
+    private UserRole userRole;
     private boolean isActive;
+    private boolean isBlocked;
+    private LocalDate creationDate;
+    private LocalDate lastLoggedDate;
 }
