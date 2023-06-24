e00533f5a665128f91b1c256a126164cdf121069
mcblankenburg <maciej.blankenburg@gmail.com>
2023-06-24 19:01:44 +0200

    in this moment username --> email

--- microservices/identity-management/src/main/java/com/skytrader24/identitymanagement/UserEntity.java
+++ microservices/identity-management/src/main/java/com/skytrader24/identitymanagement/UserEntity.java
@@ -14,9 +14,8 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String username;
-    private String password;
     private String email;
+    private String password;
     private UserRole userRole;
     private boolean isActive;
     private boolean isBlocked;
